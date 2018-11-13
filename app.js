e2c53d1c90cf0bbcb4ad69c69e53cc55d17db8ab
prcao <patrickrcao@gmail.com>
2018-11-12 21:26:24 -0500

    Fix combining react and express

--- app.js
+++ app.js
@@ -7,11 +7,7 @@ var apiRouter = require('./routes/api');
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api', apiRouter);
 
@@ -21,6 +17,9 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
   
+const port = process.env.PORT || 5000;
+app.listen(port);
 
+console.log("Server started on port 5000");
 
 module.exports = app;
